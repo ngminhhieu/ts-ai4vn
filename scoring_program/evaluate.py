fd5392e2524e7fe3517f33a11b1af7dc2b4caf8b
ngminhhieu <minhhieun4@gmail.com>
2022-07-07 14:55:29 +0700

    fix scoring program

--- scoring_program/evaluate.py
+++ scoring_program/evaluate.py
@@ -18,11 +18,6 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 #             print("Missing the column {} in the result file. Columns name must be {}, where the indexes _1, _2, etc. represent the order of the location needed to be predicted".format(gt_col, gt_columns))
 #             exit()
 
-def check_valid_file(gt_df, pred_df, i):
-    if len(pred_df) < len(gt_df):
-        print("Missing values in file res_{}.csv".format(i))
-        exit()
-
 def eval_mae(y_true, y_pred):
     return mean_absolute_error(y_true.flatten(), y_pred.flatten())
 
@@ -45,20 +40,43 @@ def count_files(folder):
             count += 1
     return count
 
+def count_folders(truth_dir):
+    # return len(os.walk(truth_dir).next()[1])
+    return len(next(os.walk(truth_dir))[1])
+
 if __name__ == "__main__":
     [_, input_dir, output_dir] = sys.argv
     submission_dir = os.path.join(input_dir, 'res')
     truth_dir = os.path.join(input_dir, 'ref')
-    num_files = count_files(truth_dir)
-    horizon=3
-    gt_np = np.zeros((num_files, horizon))
-    pred_np = np.zeros((num_files, horizon))
-    for i in range(1, num_files+1):
-        gt_df = pd.read_csv(os.path.join(truth_dir, "gt_{}.csv".format(i)), header=None)
-        pred_df = pd.read_csv(os.path.join(submission_dir, "res_{}.csv".format(i)), header=None)
-        check_valid_file(gt_df, pred_df, i)
-        gt_np[i-1, :] = gt_df.values.flatten()
-        pred_np[i-1, :] = pred_df.values.flatten()
+
+    num_files = count_files(os.path.join(truth_dir, "1"))
+    num_folders = count_folders(truth_dir)
+    horizon=24
+
+    gt_np = np.zeros((num_folders, num_files, horizon))
+    pred_np = np.zeros((num_folders, num_files, horizon))
+
+    for folder_th in range(1, num_folders+1):
+        res_folder = os.path.join(submission_dir, str(folder_th))
+        ref_folder = os.path.join(truth_dir, str(folder_th))
+        num_files_res = count_files(res_folder)
+        num_files_ref = count_files(ref_folder)
+        if num_files_ref > num_files_res:
+            print("Missing {} files in the folder res/{}".format(num_files_ref - num_files_res, folder_th))
+        elif num_files_ref < num_files_res:
+            print("Exceeding {} files in the folder res/{}".format(num_files_res - num_files_ref, folder_th))
+        else:
+            for file_th in range(1, num_files+1):
+                gt_df = pd.read_csv(os.path.join(ref_folder, "gt_{}_{}.csv".format(folder_th, file_th)), usecols=["PM2.5"])
+                pred_df = pd.read_csv(os.path.join(res_folder, "res_{}_{}.csv".format(folder_th, file_th)), usecols=["PM2.5"])
+                if len(pred_df) < len(gt_df):
+                    print("Missing {} values in the file res_{}_{}.csv".format(len(gt_df)-len(pred_df), folder_th, file_th))
+                if len(pred_df) > len(gt_df):
+                    print("Exceeding values in the file res_{}_{}.csv".format(len(pred_df)-len(gt_df), folder_th, file_th))
+                else:
+                    gt_np[folder_th-1, file_th-1, :] = gt_df.values.flatten()
+                    pred_np[folder_th-1, file_th-1, :] = pred_df.values.flatten()
+    
     res_dict = {}
     res_dict['mae'] = eval_mae(gt_np, pred_np)
     res_dict['mape'] = eval_mape(gt_np, pred_np)
