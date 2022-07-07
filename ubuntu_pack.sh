df3c9638570c256b218ed8b26620aafc1ce3385f
anhtn <tongngocanhcampha@gmail.com>
2022-07-07 19:54:30 +0700

    Zip all file csv with ubuntu environment

--- ubuntu_pack.sh
+++ ubuntu_pack.sh
@@ -5,8 +5,8 @@ rm competition.zip
 zip -j scoring_program.zip ./scoring_program/*.py
 zip -j scoring_program.zip ./scoring_program/metadata
 
-zip -j public_ground_truth.zip ./data/public/gt.csv
-zip -j private_ground_truth.zip ./data/private/gt.csv
+zip -j public_ground_truth.zip data/public/*.csv
+zip -j private_ground_truth.zip ./data/private/*.csv
 
 zip -j competition.zip html/award.html html/data.html html/evaluation.html html/overview.html html/submission.html html/terms.html
 zip -j competition.zip competition.yaml scoring_program.zip public_ground_truth.zip private_ground_truth.zip
