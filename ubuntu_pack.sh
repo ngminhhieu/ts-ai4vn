ad23af770f5dc767dec198c87fb3add8a01d773d
anhtn <tongngocanhcampha@gmail.com>
2022-07-08 00:02:22 +0700

    Full bash of ubuntu and windows

--- ubuntu_pack.sh
+++ ubuntu_pack.sh
@@ -5,8 +5,8 @@ rm competition.zip
 zip -j scoring_program.zip ./scoring_program/*.py
 zip -j scoring_program.zip ./scoring_program/metadata
 
-zip -j public_ground_truth.zip data/public/*.csv
-zip -j private_ground_truth.zip ./data/private/*.csv
+cd data/public/ && zip -r ../../public_ground_truth.zip */*.csv ; cd -
+cd data/private/ && zip -r ../../private_ground_truth.zip */*.csv ; cd -
 
 zip -j competition.zip html/award.html html/data.html html/evaluation.html html/overview.html html/submission.html html/terms.html
 zip -j competition.zip competition.yaml scoring_program.zip public_ground_truth.zip private_ground_truth.zip
--- windows_pack.sh
+++ windows_pack.sh
@@ -3,8 +3,8 @@ rm competition.zip
 7z a scoring_program.zip ./scoring_program/*.py
 7z a scoring_program.zip ./scoring_program/metadata
 
-7z a public_ground_truth.zip ./data/public/*.csv
-7z a private_ground_truth.zip ./data/private/*.csv
+7z a public_ground_truth.zip -i!./data/public/*/*.csv
+7z a private_ground_truth.zip -i!./data/private/*/*csv
 
 7z a competition.zip ./html/award.html ./html/data.html ./html/evaluation.html ./html/overview.html ./html/submission.html ./html/terms.html
 7z a competition.zip competition.yaml scoring_program.zip public_ground_truth.zip private_ground_truth.zip
